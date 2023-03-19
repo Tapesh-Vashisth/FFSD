fc01b1ce888e3868d4947715124bb7391d2c78f1
Pranav Gupta <pranav.gupta@raybusiness.onmicrosoft.com>
2023-03-19 16:33:04 +0530

    'c'

--- controllers/auth/forgotPassword.js
+++ controllers/auth/forgotPassword.js
@@ -1,11 +1,22 @@
-
+let db = require('../../database/db')
 const forgotPasswordGet = (req, res) => {
-    res.render("./Pages/forgotPassword");
+  res.render('./Pages/forgotPassword')
 }
 
 const forgotPasswordPost = (req, res) => {
-    
+  db.get(
+    'SELECT * FROM user where email=$email',
+    {
+      $email: req.body.email,
+    },
+    async (error, row) => {
+      if (!error && row) {
+        return res.render('./Pages/forgotPassword', { error: 0 })
+      }
+      return res.render('./Pages/forgotPassword', { error: 1 })
+    },
+  )
 }
 
-module.exports.get = forgotPasswordGet;
-module.exports.post = forgotPasswordPost;
+module.exports.get = forgotPasswordGet
+module.exports.post = forgotPasswordPost
